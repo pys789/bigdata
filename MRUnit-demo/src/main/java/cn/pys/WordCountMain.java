25029a6137e524febe9ff29304e3624fdbe53d7d
pengys <pys789@126.com>
2021-02-14 11:35:32 +0800

    拆分工程

--- MRUnit-demo/src/main/java/cn/pys/WordCountMain.java
+++ MRUnit-demo/src/main/java/cn/pys/WordCountMain.java
@@ -1,4 +1,4 @@
-package cn.pys.wordcount;
+package cn.pys;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
--- MRUnit-demo/src/main/java/cn/pys/WordCountMapper.java
+++ MRUnit-demo/src/main/java/cn/pys/WordCountMapper.java
@@ -1,4 +1,4 @@
-package cn.pys.wordcount;
+package cn.pys;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
